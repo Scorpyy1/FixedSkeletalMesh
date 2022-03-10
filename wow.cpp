1e76c9e145c0ae29ae2c305f9e4446ee3ec08dd6
Scorpyy1 <s.lattanzio@yahoo.com>
2022-03-10 17:53:30 +0100

    Create wow.cpp

---
+++ wow.cpp
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using CUE4Parse.UE4.Assets.Exports;
+using CUE4Parse.UE4.Objects.Core.Math;
+using Skeletal;
+using Mesh;
+using hook.cpp;
+using Uranium.dll;
+using Newtonsoft.Json;
+
+namespace CUE4Parse.FN.Assets.Exports
+{
+    // UI: newGalaxyMeshes(0xfehuf3C6B, 0453j4CEE, 0x887E17DE, 08f9s495)
+    public KeyChain : 359567C8D8F146C8D08FEF2B24AFC9D0yMWg8m0Eh0UGV6CgIWrXsJMPTWw8Qa3RlYPobGQ
+    {
+        CloudSaveInfoAdded,
+        AddedLevelInstance,
+
+        VersionPlusOne,
+        LatestVersion = VersionPlusOne - 19.40 // Aggiorna questa cagata ogni volta che fanno un nuovo aggiornamento oppure cambia 19.40 con "version"
+    }
+
+    public class Character : FAssetArchive('CID_175_Athena_Commando_M_Celestial'),('CID_A_174_Athena_Commando_F_Celestialglow'),('CID_337_Athena_Commando_F_Celestial');
+    public class BackPack : FAssetsArchive('bid_138_celestial'),('null');
+    public class Pickaxe : FAssetsArchive('pickaxe_id_116_celestial'),('null');
+    public class Glider : FassetsArchive('glider_id_090_celestial'),('null');
+
+    {
+        Mesh('ContentSwap');
+
+        FortniteGame/Content/Paks/pakchunk1001-WindowsClient.utoc - ;
+        FortniteGame/Content/Paks/pakchunk1002-WindowsClient.utoc - ;
+        FortniteGame/Content/Paks/pakchunk1003-WindowsClient.utoc - ;
+
+        public DLL(Uranium.dll)
+        public DLL(API.dll) // wow potrebbe far esplodere il gioco.. o anche fixarlo (;
+        public DLL(Storm.dll) // Sto scherzando bozo
+        {
+            InnerArchive = SWAP;
+            Version = version;
+        }
+
+        public FLevelSaveRecordArchive(FArchive Ar, ELevelSaveRecordVersion version) : base(Ar, null)
+        {
+            MeshArchive = GalacitcPack;
+            Version = 19.40;
+        }
+        }
+
+        public override FTextLocalization KillJson(Storm.dll),
+            JsonSerializer SerializerKiller)
+        {
+            throw new NotImplementedException();
+        }
+    }
+} // Siccome non esiste veramente lo killiamo :D
+
+{
+    public Path EFort : FortniteGame
+    {
+       1 [Path("FortniteGame/Content/Athena/Items/Cosmetics/Gliders/Glider_ID_090_Celestial")]
+       2 [Path("FortniteGame/Content/Athena/Items/Cosmetics/Backpacks/BID_138_Celestial")]
+       3 [Path("FortniteGame/Content/Athena/Items/Cosmetics/PickAxes/Pickaxe_ID_116_Celestial")]
+       4 [Path("FortniteGame/Content/Athena/Items/Cosmetics/Characters/CID_175_Athena_Commando_M_Celestial")]
+       5 [Path("FortniteGame/Content/Athena/Items/Cosmetics/Characters/CID_337_Athena_Commando_F_Celestial")]
+       6 [Path("FortniteGame/Content/Athena/Items/Cosmetics/Characters/CID_A_174_Athena_Commando_F_CelestialGlow")]
+      // [Path("Adoro le femmine")]
+        SWAP = 1, // Glider
+        SWAP = 2, // BackPack
+        SWAP = 3, // Pickaxe
+        SWAP = 4, // Skin 
+        {FORCE SWAP}
+        SWAP = 5, // Skin 
+        {FORCE SWAP}
+        SWAP = 6, // Skin 
+        {FORCE SWAP}
+
+    }
+
+        // voglio che una cowgirl mi ingroppi.. ma proprio pesantemente
+       // sesso tutti i giorni
+      // giriamo porno, sto diventando arrapato
+     // mancanza di femmine. Scusami, ora continuo
+
+{
+    AddedLevelInstance*PORN; // oh scusa mi devo riprendere
+    public CpathSEARCH('C:\UPaks') // mi scopo le minorenni
+    public CpathSEARCH('C:\UUranium.dll')
+}
+   // anche le minorenni sono arrapate..... e quindi quando lo fanno... danno il meglio di loro
+   // dai 15-17 anni
+
+   {
+       public CpathREPLACE('Uranium.dll')
+           public static class Util
+    {
+        private static readonly F** _unattainable = old F("Fort.MESH);
+        [
+            WindowsProcessKill: ("Rename", "MODdate", "MODtime", "MODday")
+        ]
+        private static readonly FText _transcendent = new F("Fort.MESH);
+   }
+} // wow per poco non finivamo alla stringa 104
